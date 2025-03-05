1b13c938f6383a3d2bd7a3bd5b6e5509b7ee73cb
Elieser Capillar <elieser.capillar@hotmail.com>
2025-03-04 23:44:42 -0600

    Added Hero Section

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -4,8 +4,17 @@
     <div>
       <!-- <RippleCircleBG/> -->
       <div>
-        <NavBar :activeIndex="0" />
+        <NavBar :activeIndex="0"/>
         <!-- <NavBarAlt /> -->
+        <!-- Hero Section -->
+        <section>
+          <div class="relative">
+            <img src="/hero_bg.png" class="block w-full">
+            <div class="w-full absolute top-0 left-0 text-center">
+              <h1 class="relative text-[#ff0000] text-center text-9xl font-bold" >ARE YOU HENEHOE ENOUGH?</h1>
+            </div>
+          </div>
+        </section>
         <HeroSection />
         <Technologies />
         <Experience />
@@ -18,5 +27,4 @@
 
 <script setup lang="ts">
 import NavBar from "@/components/NavBar.vue";
-import NavBarAlt from "@/components/NavBarAlt.vue";
 </script>
