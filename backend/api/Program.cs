286bb94d092cf4a6191edbb18692771d6bd4945a
Elieser Capillar <elieser.capillar@hotmail.com>
2025-04-18 01:45:10 -0400

    Changed folder structure

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -18,9 +18,6 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<MapleTinderDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<CharacterScraper>();
-//builder.Services.AddHostedService<CharacterScraperService>();
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -39,18 +36,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-//add argument when calling docker run or include commandLineArgs in launchSettings.json.
-//Or just leave as it always try to install
-if (args.Length > 0 && args[0] == "-i")
-    PerformInitialPlaywrightInstall();
-
-
-static void PerformInitialPlaywrightInstall()
-{
-    var exitCode = Microsoft.Playwright.Program.Main(new[] { "install", "--with-deps", "chromium" });
-    if (exitCode != 0)
-    {
-        throw new Exception($"Playwright exited with code {exitCode}");
-    }
-}
