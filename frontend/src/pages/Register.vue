ce46bf9cffe729a549a4bb652ce4ed28efec7c2a
Elieser Capillar <elieser.capillar@hotmail.com>
2025-05-16 21:55:35 -0400

    Register UX

--- frontend/src/pages/Register.vue
+++ frontend/src/pages/Register.vue
@@ -12,18 +12,18 @@
             <div class="grid items-center w-full gap-4">
               <div class="flex flex-col space-y-1.5">
                 <Label for="name">Email</Label>
-                <Input id="email" type="email" v-model="email"/>
-                <span class="text-sm" :class="hasUppercase ? 'text-green-500' : 'text-red-500'">Enter a valid email.</span>
+                <Input type="email" v-model="email"/>
+                <span class="text-sm" :class="isValidEmail ? 'text-green-500' : 'text-red-500'">Enter a valid email.</span>
               </div>
               <div class="flex flex-col space-y-1.5">
                 <Label for="framework">Password</Label>
-                <Input id="password" type="password" v-model="password"/>
+                <Input type="password" v-model="password"/>
               </div>
               <div class="flex flex-col space-y-1.5">
                 <Label for="framework">Confim Password</Label>
-                <Input id="password" type="password" v-model="confirmPassword"/>
+                <Input type="password" v-model="confirmPassword"/>
               </div>
-              <ul class="text-sm mt-2 text-left space-y-1">
+              <ul v-if="badPassword" class="text-sm mt-2 text-left space-y-1">
                 <li :class="hasUppercase ? 'text-green-500' : 'text-red-500'">• At least one uppercase letter</li>
                 <li :class="hasLowercase ? 'text-green-500' : 'text-red-500'">• At least one lowercase letter</li>
                 <li :class="hasDigit ? 'text-green-500' : 'text-red-500'">• At least one number</li>
@@ -33,12 +33,31 @@
               </ul>
             </div>
           </form>
-          <Button @click="register" class="w-full mb-6">Create Account</Button>
+          <Button @click="register" class="w-full mb-6" type="submit" :disabled=!isValidForm>Create Account</Button>
+          <div class="flex items-center mb-6">
+                <div class="border-b-[1px] flex-1"></div>
+                <span class="mx-2 flex-1 text-center">Or Sign in with</span>
+                <div class="border-b-[1px] flex-1"></div>
+              </div>
+              <div class="flex items-center justify-center gap-2">
+                <Button variant="outline" class="w-full mb-6">
+                  <Icon icon="simple-icons:google"/>Google
+                </Button>
+                <Button variant="outline" class="w-full mb-6">
+                  <Icon icon="simple-icons:discord"/>Discord
+                </Button>
+              </div>
         </CardContent>
-        <CardFooter class="flex justify-center px-6 pb-6">
+        <CardFooter class="flex flex-col justify-center px-6 pb-6">
           <span class="text-sm">
             Already have an account?
             <a href="/login" class="text-blue-400">Sign in!</a>
+          </span>
+          <span class="text-xs">
+            By creating an account, you agree to our 
+            <a href="/terms-" class="text-blue-400">Terms of Use</a>
+            and
+            <a href="/privacy-policy" class="text-blue-400">Privacy Policy</a>.
           </span>
         </CardFooter>
       </Card>
@@ -73,6 +92,8 @@ const hasSpecialChar = computed(() => /[^a-zA-Z0-9]/.test(password.value))
 const isLongEnough = computed(() => password.value.length >= 8)
 const doesPassMatch = computed(() => password.value == confirmPassword.value)
 
+const isValidEmail = computed(() => /\S+@\S+\.\S+/.test(email.value))
+
 const isPasswordValid = computed(() =>
   hasUppercase.value &&
   hasLowercase.value &&
@@ -81,10 +102,17 @@ const isPasswordValid = computed(() =>
   isLongEnough.value
 )
 
+const doPasswordsMatch = computed(() => password.value == confirmPassword.value)
+
+const isValidForm = computed(() => isValidEmail && password.value.length > 0 && confirmPassword.value.length)
+
+const badPassword = ref(false);
+
 async function register() {
   try {
-    if (!isPasswordValid.value) {
-        error.value = 'Password does not meet the required criteria.'
+    if (!isPasswordValid.value || !doPasswordsMatch.value) {
+        badPassword.value = true;
+        // error.value = 'Password does not meet the required criteria.'
         return
     }
 
