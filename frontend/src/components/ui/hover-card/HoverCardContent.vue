52f561102b295e0ce80b4beab0a1f01cbb71a79b
Elieser Capillar <elieser.capillar@hotmail.com>
2025-06-17 04:42:58 -0400

    Added shadcn components

---
+++ frontend/src/components/ui/hover-card/HoverCardContent.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import {
+  HoverCardContent,
+  type HoverCardContentProps,
+  HoverCardPortal,
+  useForwardProps,
+} from 'reka-ui'
+import { cn } from '@/lib/utils'
+
+const props = withDefaults(
+  defineProps<HoverCardContentProps & { class?: HTMLAttributes['class'] }>(),
+  {
+    sideOffset: 4,
+  },
+)
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <HoverCardPortal>
+    <HoverCardContent
+      v-bind="forwardedProps"
+      :class="
+        cn(
+          `z-50 w-64 p-4 
+           rounded-md border 
+           bg-popover text-popover-foreground 
+           shadow-md outline-none
+           
+           data-[state=open]:animate-in 
+           data-[state=closed]:animate-out 
+           data-[state=closed]:fade-out-0 
+           data-[state=open]:fade-in-0 
+           data-[state=closed]:zoom-out-95 
+           data-[state=open]:zoom-in-95 
+           data-[side=bottom]:slide-in-from-top-2 
+           data-[side=left]:slide-in-from-right-2 
+           data-[side=right]:slide-in-from-left-2 
+           data-[side=top]:slide-in-from-bottom-2`,
+          props.class,
+        )
+      "
+    >
+      <slot />
+    </HoverCardContent>
+  </HoverCardPortal>
+</template>
+
+<style scoped>
+.hover-card-content[data-state="open"].animate-in {
+  animation-duration: 70ms !important;
+}
+
+.hover-card-content[data-state="closed"].animate-out {
+  animation-duration: 0ms !important;
+}
+</style>
--- frontend/src/pages/Leaderboard.vue
+++ frontend/src/pages/Leaderboard.vue
@@ -2,7 +2,7 @@
   <main class="w-full h-screen bg-black-grey-radial text-white flex flex-col items-center justify-center">
     <h1 class="font-medium text-4xl text-center py-4">The Hottest Characters of the week!</h1>
     <div class="w-[60rem]">
-      <Table>
+      <Table class="select-none">
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
@@ -15,9 +15,16 @@
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow v-for="(entry, id) in leaderboardStore.likeCharacters" :key="id">
+          <TableRow v-for="(entry, id) in testList" :key="id">
             <TableCell class="font-medium">{{ entry.character.id }}</TableCell>
-            <TableCell>{{ entry.character.name }}</TableCell>
+            <TableCell>
+              <HoverCard>
+                <HoverCardTrigger class="text-blue-400">{{ entry.character.name }}</HoverCardTrigger>
+                <HoverCardContent class="h-64 w-64 p-0">
+                  <img class="h-64 w-64" :src="entry.character.imageUrl">
+                </HoverCardContent>
+              </HoverCard>
+            </TableCell>
             <TableCell>{{ entry.character.level }}</TableCell>
             <TableCell>{{ entry.character.job }}</TableCell>
             <TableCell>{{ entry.character.world }}</TableCell>
@@ -42,6 +49,11 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card'
 
 const leaderboardStore = useLeaderboardStore();
 const showLikes = ref(true);
@@ -62,7 +74,7 @@ function showLastPage() {
   fetchPage();
 }
 
-const invoices = [
+const testList = [
   {
     totalCount: '100',
     character: {
@@ -75,40 +87,103 @@ const invoices = [
     }
   },
   {
-    invoice: 'INV002',
-    paymentStatus: 'Pending',
-    totalAmount: '$150.00',
-    paymentMethod: 'PayPal',
+    totalCount: '100',
+    character: {
+      id: 2,
+      name: "CHARACTER 2",
+      level: 300,
+      job: "Dual Blade",
+      world: "Kronos",
+      imageUrl: "https://i.mapleranks.com/u/HJPKNMCLGHPGJFODCAGJAKPKCDLCJBCAACIJKMBDGDCBGGDPLIMAJLEGNJKKMNLGDABBGLGNNKLJCIJHCGMKEPHAIKNAJKFHPJPDHPCDGBGLPGCCNCBJFEHGKHHCJOLJPDNKFNAKCPAFDICIKJMMGJCKIJPCLLNHEAILIKPGNALMAOIHEBFDPHHOAOGCBKMBMPFBPKMOFHNFAJLDNAKPOPCOJNLEGOLKHJDFMLDBJDBFFIKFIMJNFPJAMAIFOFLC.png",
+    }
   },
   {
-    invoice: 'INV003',
-    paymentStatus: 'Unpaid',
-    totalAmount: '$350.00',
-    paymentMethod: 'Bank Transfer',
+    totalCount: '100',
+    character: {
+      id: 3,
+      name: "CHARACTER 3",
+      level: 300,
+      job: "Dual Blade",
+      world: "Kronos",
+      imageUrl: "https://i.mapleranks.com/u/HJPKNMCLGHPGJFODCAGJAKPKCDLCJBCAACIJKMBDGDCBGGDPLIMAJLEGNJKKMNLGDABBGLGNNKLJCIJHCGMKEPHAIKNAJKFHPJPDHPCDGBGLPGCCNCBJFEHGKHHCJOLJPDNKFNAKCPAFDICIKJMMGJCKIJPCLLNHEAILIKPGNALMAOIHEBFDPHHOAOGCBKMBMPFBPKMOFHNFAJLDNAKPOPCOJNLEGOLKHJDFMLDBJDBFFIKFIMJNFPJAMAIFOFLC.png",
+    }
   },
   {
-    invoice: 'INV004',
-    paymentStatus: 'Paid',
-    totalAmount: '$450.00',
-    paymentMethod: 'Credit Card',
+    totalCount: '100',
+    character: {
+      id: 4,
+      name: "CHARACTER 4",
+      level: 300,
+      job: "Dual Blade",
+      world: "Kronos",
+      imageUrl: "https://i.mapleranks.com/u/HJPKNMCLGHPGJFODCAGJAKPKCDLCJBCAACIJKMBDGDCBGGDPLIMAJLEGNJKKMNLGDABBGLGNNKLJCIJHCGMKEPHAIKNAJKFHPJPDHPCDGBGLPGCCNCBJFEHGKHHCJOLJPDNKFNAKCPAFDICIKJMMGJCKIJPCLLNHEAILIKPGNALMAOIHEBFDPHHOAOGCBKMBMPFBPKMOFHNFAJLDNAKPOPCOJNLEGOLKHJDFMLDBJDBFFIKFIMJNFPJAMAIFOFLC.png",
+    }
   },
   {
-    invoice: 'INV005',
-    paymentStatus: 'Paid',
-    totalAmount: '$550.00',
-    paymentMethod: 'PayPal',
+    totalCount: '100',
+    character: {
+      id: 5,
+      name: "CHARACTER 5",
+      level: 300,
+      job: "Dual Blade",
+      world: "Kronos",
+      imageUrl: "https://i.mapleranks.com/u/HJPKNMCLGHPGJFODCAGJAKPKCDLCJBCAACIJKMBDGDCBGGDPLIMAJLEGNJKKMNLGDABBGLGNNKLJCIJHCGMKEPHAIKNAJKFHPJPDHPCDGBGLPGCCNCBJFEHGKHHCJOLJPDNKFNAKCPAFDICIKJMMGJCKIJPCLLNHEAILIKPGNALMAOIHEBFDPHHOAOGCBKMBMPFBPKMOFHNFAJLDNAKPOPCOJNLEGOLKHJDFMLDBJDBFFIKFIMJNFPJAMAIFOFLC.png",
+    }
   },
   {
-    invoice: 'INV006',
-    paymentStatus: 'Pending',
-    totalAmount: '$200.00',
-    paymentMethod: 'Bank Transfer',
+    totalCount: '100',
+    character: {
+      id: 6,
+      name: "CHARACTER 6",
+      level: 300,
+      job: "Dual Blade",
+      world: "Kronos",
+      imageUrl: "https://i.mapleranks.com/u/HJPKNMCLGHPGJFODCAGJAKPKCDLCJBCAACIJKMBDGDCBGGDPLIMAJLEGNJKKMNLGDABBGLGNNKLJCIJHCGMKEPHAIKNAJKFHPJPDHPCDGBGLPGCCNCBJFEHGKHHCJOLJPDNKFNAKCPAFDICIKJMMGJCKIJPCLLNHEAILIKPGNALMAOIHEBFDPHHOAOGCBKMBMPFBPKMOFHNFAJLDNAKPOPCOJNLEGOLKHJDFMLDBJDBFFIKFIMJNFPJAMAIFOFLC.png",
+    }
   },
   {
-    invoice: 'INV007',
-    paymentStatus: 'Unpaid',
-    totalAmount: '$300.00',
-    paymentMethod: 'Credit Card',
+    totalCount: '100',
+    character: {
+      id: 7,
+      name: "CHARACTER 7",
+      level: 300,
+      job: "Dual Blade",
+      world: "Kronos",
+      imageUrl: "https://i.mapleranks.com/u/HJPKNMCLGHPGJFODCAGJAKPKCDLCJBCAACIJKMBDGDCBGGDPLIMAJLEGNJKKMNLGDABBGLGNNKLJCIJHCGMKEPHAIKNAJKFHPJPDHPCDGBGLPGCCNCBJFEHGKHHCJOLJPDNKFNAKCPAFDICIKJMMGJCKIJPCLLNHEAILIKPGNALMAOIHEBFDPHHOAOGCBKMBMPFBPKMOFHNFAJLDNAKPOPCOJNLEGOLKHJDFMLDBJDBFFIKFIMJNFPJAMAIFOFLC.png",
+    }
+  },
+  {
+    totalCount: '100',
+    character: {
+      id: 8,
+      name: "CHARACTER 8",
+      level: 300,
+      job: "Dual Blade",
+      world: "Kronos",
+      imageUrl: "https://i.mapleranks.com/u/HJPKNMCLGHPGJFODCAGJAKPKCDLCJBCAACIJKMBDGDCBGGDPLIMAJLEGNJKKMNLGDABBGLGNNKLJCIJHCGMKEPHAIKNAJKFHPJPDHPCDGBGLPGCCNCBJFEHGKHHCJOLJPDNKFNAKCPAFDICIKJMMGJCKIJPCLLNHEAILIKPGNALMAOIHEBFDPHHOAOGCBKMBMPFBPKMOFHNFAJLDNAKPOPCOJNLEGOLKHJDFMLDBJDBFFIKFIMJNFPJAMAIFOFLC.png",
+    }
+  },
+  {
+    totalCount: '100',
+    character: {
+      id: 9,
+      name: "CHARACTER 9",
+      level: 300,
+      job: "Dual Blade",
+      world: "Kronos",
+      imageUrl: "https://i.mapleranks.com/u/HJPKNMCLGHPGJFODCAGJAKPKCDLCJBCAACIJKMBDGDCBGGDPLIMAJLEGNJKKMNLGDABBGLGNNKLJCIJHCGMKEPHAIKNAJKFHPJPDHPCDGBGLPGCCNCBJFEHGKHHCJOLJPDNKFNAKCPAFDICIKJMMGJCKIJPCLLNHEAILIKPGNALMAOIHEBFDPHHOAOGCBKMBMPFBPKMOFHNFAJLDNAKPOPCOJNLEGOLKHJDFMLDBJDBFFIKFIMJNFPJAMAIFOFLC.png",
+    }
+  },
+  {
+    totalCount: '100',
+    character: {
+      id: 10,
+      name: "CHARACTER 10",
+      level: 300,
+      job: "Dual Blade",
+      world: "Kronos",
+      imageUrl: "https://i.mapleranks.com/u/HJPKNMCLGHPGJFODCAGJAKPKCDLCJBCAACIJKMBDGDCBGGDPLIMAJLEGNJKKMNLGDABBGLGNNKLJCIJHCGMKEPHAIKNAJKFHPJPDHPCDGBGLPGCCNCBJFEHGKHHCJOLJPDNKFNAKCPAFDICIKJMMGJCKIJPCLLNHEAILIKPGNALMAOIHEBFDPHHOAOGCBKMBMPFBPKMOFHNFAJLDNAKPOPCOJNLEGOLKHJDFMLDBJDBFFIKFIMJNFPJAMAIFOFLC.png",
+    }
   },
 ]
 </script>
