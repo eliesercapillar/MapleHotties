73fee3f42a50585a73bb7e870396be15e2efcc8e
Elieser Capillar <elieser.capillar@hotmail.com>
2025-06-25 04:33:34 -0400

    Landing page final v1

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <footer class="bg-black-grey-radial h-64 text-white">
-    <div class="mx-32 py-8">
+  <footer class="bg-black-grey-radial h-60 text-white">
+    <div class="mx-96 py-8">
       <div class="flex items-center justify-between border-b-2 border-zinc-500 pb-2">
         <div>
           <div class="flex items-center gap-2">
@@ -8,17 +8,19 @@
             <span class="text-2xl">MapleHotties</span>
           </div>
           <p class="text-md my-2">The hottest players in Maplestory GMS.</p>
-          <Socials class="pt-6"/>
+          <Socials class="pt-2"/>
         </div>
         <div class="flex flex-col items-start justify-center gap-2">
           <RouterLink to="/faq">FAQ</RouterLink>
+          <RouterLink to="/cookies">Cookies</RouterLink>
           <RouterLink to="/privacy">Privacy Policy</RouterLink>
         </div>
-        <div>
-          <RouterLink to="/">FAQ</RouterLink>
-        </div>
-        <div>
-          <RouterLink to="/">FAQ</RouterLink>
+      </div>
+      <div class="pt-2 flex justify-between">
+        <p class="font-thin text-xs">© 2025 maplehotties.com | All rights reserved.</p>
+        <div class="text-right">
+          <p class="font-thin text-xs">Made with ❤️ by KPJS</p>
+          <p class="font-thin text-xs pt-1">Any suggestions? Join the <a href="@" class="text-blue-400">community discord!</a></p>
         </div>
       </div>
       <!-- <div class="flex items-center justify-between border-b-2 border-foreground">
