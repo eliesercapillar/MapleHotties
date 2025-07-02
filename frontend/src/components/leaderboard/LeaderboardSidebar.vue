3218df597d95a29d63437729c00599e912d80498
Elieser Capillar <elieser.capillar@hotmail.com>
2025-07-02 05:46:47 -0400

    Added select jobs

--- frontend/src/components/leaderboard/LeaderboardSidebar.vue
+++ frontend/src/components/leaderboard/LeaderboardSidebar.vue
@@ -8,8 +8,7 @@
       <button class="rounded-full bg-button_primary hover:text-button_highlight hover:scale-105 p-3" @click=""><Icon icon="line-md:log-out" class="scale-[1.25]"/></button>
     </nav>
     <div class="bg-[#111418] flex flex-col items-start">
-      <div class="w-full mt-2 p-2 flex items-center justify-evenly gap-4">
-        <span class="font-bold text-md">Sort by:</span>
+      <div id="type" class="w-full p-2">
         <div class="flex items-center justify-evenly gap-2">
           <Button class="bg-like-gradient" @click="toggleView(true)">Hotties</Button>
           <Button class="bg-nope-gradient" @click="toggleView(false)">Notties</Button>
@@ -18,22 +17,66 @@
       <div id="search" class="w-full p-2">
         <Input type="text" placeholder="Search for a character..." class="text-black"/>
       </div>
-      <div id="filters" class="w-full p-2">
+      <div id="time" class="w-full p-2">
         <span class="font-bold text-md">Time:</span>
         <RadioGroup default-value="all" class="flex justify-evenly mt-2">
           <div class="flex items-center gap-2">
-            <RadioGroupItem id="r1" value="weekly" class="border-white text-white"/>
-            <Label for="r1">Weekly</Label>
+            <RadioGroupItem id="weekly" value="weekly" class="border-white text-white"/>
+            <Label for="weekly">Weekly</Label>
           </div>
           <div class="flex items-center space-x-2">
-            <RadioGroupItem id="r2" value="monthly" class="border-white text-white"/>
-            <Label for="r2">Monthly</Label>
+            <RadioGroupItem id="monthly" value="monthly" class="border-white text-white"/>
+            <Label for="monthly">Monthly</Label>
           </div>
           <div class="flex items-center space-x-2">
-            <RadioGroupItem id="r3" value="all" class="border-white text-white"/>
-            <Label for="r3">All Time</Label>
+            <RadioGroupItem id="all" value="all" class="border-white text-white"/>
+            <Label for="all">All Time</Label>
           </div>
         </RadioGroup>
+      </div>
+      <div class="w-full p-2">
+        <Select class="text-black">
+          <SelectTrigger class="w-[280px]">
+            <SelectValue placeholder="Search by class" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="beginner">
+                Beginner
+              </SelectItem>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>Warrior</SelectLabel>
+              <SelectItem v-for="job in Jobs.warriors" :value="job.value">
+                {{job.className}}
+              </SelectItem>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>Magician</SelectLabel>
+              <SelectItem v-for="job in Jobs.magicians" :value="job.value">
+                {{job.className}}
+              </SelectItem>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>Archer</SelectLabel>
+              <SelectItem v-for="job in Jobs.archers" :value="job.value">
+                {{job.className}}
+              </SelectItem>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>Thief</SelectLabel>
+              <SelectItem v-for="job in Jobs.thiefs" :value="job.value">
+                {{job.className}}
+              </SelectItem>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>Pirate</SelectLabel>
+              <SelectItem v-for="job in Jobs.pirates" :value="job.value">
+                {{job.className}}
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
     </div>
   </aside>
@@ -47,6 +90,16 @@ import Button from "@/components/ui/button/Button.vue";
 import Checkbox from "../ui/checkbox/Checkbox.vue";
 import { Label } from '@/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select'
+import Jobs from "@/data/Jobs.json";
 import router from "@/router"
 
 const leaderboardStore = useLeaderboardStore();
