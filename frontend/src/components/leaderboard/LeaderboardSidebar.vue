c4ec58404f185daa700dd2cd01072d28b1a7fa64
Elieser Capillar <elieser.capillar@hotmail.com>
2025-07-13 23:01:49 -0400

    Finished Leaderboard UI

--- frontend/src/components/leaderboard/LeaderboardSidebar.vue
+++ frontend/src/components/leaderboard/LeaderboardSidebar.vue
@@ -7,82 +7,161 @@
       <button class="rounded-full bg-button_primary hover:text-favourite hover:scale-105 p-3" @click=""><Icon icon="icon-park-solid:five-star-badge" class="scale-[1.25]"/></button>
       <button class="rounded-full bg-button_primary hover:text-button_highlight hover:scale-105 p-3" @click=""><Icon icon="line-md:log-out" class="scale-[1.25]"/></button>
     </nav>
-    <div class="bg-[#111418] flex flex-col items-start">
-      <div id="type" class="w-full p-2">
-        <div class="flex items-center justify-evenly gap-2">
-          <Button class="bg-like-gradient" @click="toggleView(true)">Hotties</Button>
-          <Button class="bg-nope-gradient" @click="toggleView(false)">Notties</Button>
+    <div class="bg-[#111418] flex flex-col justify-between px-2 h-[calc(100vh-var(--sidebar-nav-h))]">
+      <div>
+        <div id="search" class="w-full p-2">
+          <Input type="text" placeholder="Search for a character..." class="text-black"/>
+        </div>
+        <div class="w-full p-2">
+          <span class="font-bold text-md">Search for:</span>
+          <RadioGroup default-value="hotties" class="flex justify-evenly mt-2">
+            <div class="flex items-center gap-2">
+              <RadioGroupItem id="hotties" value="hotties" class="border-white text-white"/>
+              <Label for="hotties">Hotties</Label>
+            </div>
+            <div class="flex items-center space-x-2">
+              <RadioGroupItem id="notties" value="notties" class="border-white text-white"/>
+              <Label for="notties">Notties</Label>
+            </div>
+            <div class="flex items-center space-x-2">
+              <RadioGroupItem id="favourites" value="favourites" class="border-white text-white"/>
+              <Label for="favourites">Favourites</Label>
+            </div>
+          </RadioGroup>
+        </div>
+        <div class="w-full p-2">
+          <span class="font-bold text-md">Time:</span>
+          <RadioGroup default-value="all" class="flex justify-evenly mt-2">
+            <div class="flex items-center gap-2">
+              <RadioGroupItem id="weekly" value="weekly" class="border-white text-white"/>
+              <Label for="weekly">Weekly</Label>
+            </div>
+            <div class="flex items-center space-x-2">
+              <RadioGroupItem id="monthly" value="monthly" class="border-white text-white"/>
+              <Label for="monthly">Monthly</Label>
+            </div>
+            <div class="flex items-center space-x-2">
+              <RadioGroupItem id="all" value="all" class="border-white text-white"/>
+              <Label for="all">All Time</Label>
+            </div>
+          </RadioGroup>
+        </div>
+        <div class="w-full p-2 text-black">
+          <span class="font-bold text-md text-white">Class:</span>
+          <Select class="mt-2" default-value="all">
+            <SelectTrigger class="w-full">
+              <SelectValue placeholder="Specify a class?" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>OTHER</SelectLabel>
+                <SelectItem value="all">
+                  All
+                </SelectItem>
+                <SelectItem value="beginner">
+                  Beginner
+                </SelectItem>
+              </SelectGroup>
+              <SelectGroup>
+                <SelectLabel>WARRIOR</SelectLabel>
+                <SelectItem v-for="job in Jobs.warriors" :value="job.value">
+                  {{job.className}}
+                </SelectItem>
+              </SelectGroup>
+              <SelectGroup>
+                <SelectLabel>MAGICIAN</SelectLabel>
+                <SelectItem v-for="job in Jobs.magicians" :value="job.value">
+                  {{job.className}}
+                </SelectItem>
+              </SelectGroup>
+              <SelectGroup>
+                <SelectLabel>ARCHER</SelectLabel>
+                <SelectItem v-for="job in Jobs.archers" :value="job.value">
+                  {{job.className}}
+                </SelectItem>
+              </SelectGroup>
+              <SelectGroup>
+                <SelectLabel>THIEF</SelectLabel>
+                <SelectItem v-for="job in Jobs.thiefs" :value="job.value">
+                  {{job.className}}
+                </SelectItem>
+              </SelectGroup>
+              <SelectGroup>
+                <SelectLabel>PIRATE</SelectLabel>
+                <SelectItem v-for="job in Jobs.pirates" :value="job.value">
+                  {{job.className}}
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div class="w-full p-2 text-black">
+          <span class="font-bold text-md text-white">World:</span>
+          <Select class="mt-2" default-value="all">
+            <SelectTrigger class="w-full">
+              <SelectValue placeholder="Specify a world?" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>OTHER</SelectLabel>
+                <SelectItem value="all">
+                  All
+                </SelectItem>
+              </SelectGroup>
+              <SelectGroup>
+                <SelectLabel>INTERACTIVE</SelectLabel>
+                <SelectItem value="scania">
+                  Scania
+                </SelectItem>
+                <SelectItem value="bera">
+                  Bera
+                </SelectItem>
+                <SelectItem value="aurora">
+                  Aurora
+                </SelectItem>
+                <SelectItem value="elysium">
+                  Elysium
+                </SelectItem>
+              </SelectGroup>
+              <SelectGroup>
+                <SelectLabel>HEROIC</SelectLabel>
+                <SelectItem value="kronos">
+                  Kronos
+                </SelectItem>
+                <SelectItem value="hyperion">
+                  Hyperion
+                </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div class="w-full p-2 flex justify-end">
+          <Button class="bg-nope-gradient font-bold">Search</Button>
         </div>
       </div>
-      <div id="search" class="w-full p-2">
-        <Input type="text" placeholder="Search for a character..." class="text-black"/>
-      </div>
-      <div id="time" class="w-full p-2">
-        <span class="font-bold text-md">Time:</span>
-        <RadioGroup default-value="all" class="flex justify-evenly mt-2">
+      <footer class="w-full p-2 text-white">
+        <div>
           <div class="flex items-center gap-2">
-            <RadioGroupItem id="weekly" value="weekly" class="border-white text-white"/>
-            <Label for="weekly">Weekly</Label>
+            <img src="/logos/mt_mushroom.png" class="w-10 h-10"/>
+            <span class="text-2xl">MapleHotties</span>
           </div>
-          <div class="flex items-center space-x-2">
-            <RadioGroupItem id="monthly" value="monthly" class="border-white text-white"/>
-            <Label for="monthly">Monthly</Label>
+          <p class="text-md my-2">The hottest players in Maplestory GMS.</p>
+          <Socials class="pt-2"/>
+        </div>
+        <div class="pt-2">
+          <p class="font-thin text-xs">© 2025 maplehotties.com | All rights reserved.</p>
+          <div class="pt-1">
+            <p class="font-thin text-xs">Made with ❤️ by KPJS</p>
+            <p class="font-thin text-xs pt-1">Any suggestions? Join the <a href="@" class="text-blue-400">community Discord server!</a></p>
           </div>
-          <div class="flex items-center space-x-2">
-            <RadioGroupItem id="all" value="all" class="border-white text-white"/>
-            <Label for="all">All Time</Label>
-          </div>
-        </RadioGroup>
-      </div>
-      <div class="w-full p-2">
-        <Select class="text-black">
-          <SelectTrigger class="w-[280px]">
-            <SelectValue placeholder="Search by class" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="beginner">
-                Beginner
-              </SelectItem>
-            </SelectGroup>
-            <SelectGroup>
-              <SelectLabel>Warrior</SelectLabel>
-              <SelectItem v-for="job in Jobs.warriors" :value="job.value">
-                {{job.className}}
-              </SelectItem>
-            </SelectGroup>
-            <SelectGroup>
-              <SelectLabel>Magician</SelectLabel>
-              <SelectItem v-for="job in Jobs.magicians" :value="job.value">
-                {{job.className}}
-              </SelectItem>
-            </SelectGroup>
-            <SelectGroup>
-              <SelectLabel>Archer</SelectLabel>
-              <SelectItem v-for="job in Jobs.archers" :value="job.value">
-                {{job.className}}
-              </SelectItem>
-            </SelectGroup>
-            <SelectGroup>
-              <SelectLabel>Thief</SelectLabel>
-              <SelectItem v-for="job in Jobs.thiefs" :value="job.value">
-                {{job.className}}
-              </SelectItem>
-            </SelectGroup>
-            <SelectGroup>
-              <SelectLabel>Pirate</SelectLabel>
-              <SelectItem v-for="job in Jobs.pirates" :value="job.value">
-                {{job.className}}
-              </SelectItem>
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
+        </div>
+      </footer>
     </div>
   </aside>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { useLeaderboardStore } from "@/stores/leaderboardStore";
 import { Input } from '@/components/ui/input'
 import { Icon } from "@iconify/vue/dist/iconify.js";
@@ -101,6 +180,10 @@ import {
 } from '@/components/ui/select'
 import Jobs from "@/data/Jobs.json";
 import router from "@/router"
+import Socials from "@/components/Socials.vue";
+import { RouterLink } from "vue-router";
+
+const showStats = ref(false);
 
 const leaderboardStore = useLeaderboardStore();
 
