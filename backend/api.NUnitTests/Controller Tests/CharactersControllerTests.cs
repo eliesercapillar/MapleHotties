7a63852fd35074589c09768bec86ef4681cc696a
Elieser Capillar <elieser.capillar@hotmail.com>
2025-09-28 22:42:48 -0400

    Added new csproj for unit tests

---
+++ backend/api.NUnitTests/Controller Tests/CharactersControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using api.Controllers;
+using api.DTOs;
+using api.Interfaces;
+using MapleTinder.Shared.Data;
+
+namespace api.NUnitTests.Controller_Tests
+{
+    public class CharactersControllerTests
+    {
+        [Test]
+        public void TestGetCharactersCorrectPagination()
+        {
+            // Assign
+
+            // Act
+
+            // Assert
+
+            Assert.Pass();
+        }
+    }
+}
